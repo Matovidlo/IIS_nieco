9354281f13e9838e101e2b4284eb50c6f0b4762f
Matovidlo <matovidlo2@gmail.com>
2017-11-23 14:36:13 +0100

    Difference between users added, waiting for frontend. Session done

--- html/php/class.php
+++ html/php/class.php
@@ -2,14 +2,13 @@
 
 class Login
 {
-// 	/* TODO session */
 	private $login;
 	private $password;
 
-	public function __construct()
+	public function __construct($login = "", $password = "")
 	{
-		$this->login = $_POST["input_login"];
-		$this->password = $_POST["input_password"];
+		$this->login = $login;
+		$this->password = $password;
 	}
 
 	public function compare_password()
@@ -22,10 +21,24 @@ class Login
 
 		if (mysqli_num_rows($result) > 0) {
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo "login: " .  $row["Login"] . " heslo " . $row["Heslo"] . "<br>";
+				if ($row["Login"] === $this->login) {
+					$this->password = base64_encode(hash("sha256", $this->password, true));
+					if ($this->password === $row["Heslo"]) {
+						// session_start();
+						$_SESSION['login'] = $_POST["input_login"];
+						if (!isset($_SESSION['start_time'])) {
+							$str_time = time();
+							$_SESSION['timestamp'] = $str_time;
+							$_SESSION['logged_in'] = true;
+						}
+						// TODO session
+						return 2;
+					}
+					return 1;
+				}
 			}
 		}
-		return true;
+		return 0;
 	}
 
 	public function get_login()
@@ -38,11 +51,64 @@ class Login
 		return $password;
 	}
 
+	public function check_session() {
+		if(time() - $_SESSION['timestamp'] > 900) {
+			echo"<script>alert('15 Minutes over!');</script>";
+			// unset($_SESSION['username'], $_SESSION['password'], $_SESSION['timestamp']);
+			session_unset();
+			$_SESSION['logged_in'] = false;
+			// header("Location: http://www.stud.fit.vutbr.cz/~xvasko12/IIS/"); //redirect to index.php
+		} else {
+		}
+		// end session
+	}
+	/* return "administrator", "garant" or "student" */
+	public function get_user() {
+		// session is for administrator
+		// TODO
+		if ($_SESSION['login'] == "admin") {
+			return "administrator";
+		}
+
+		require_once("./database/mysql_init.php");
+		$mysql = new mysql_class();
+		$mysql = $mysql->get_status();
+
+		$query = "SELECT Login FROM Osoba NATURAL JOIN Zamestnanec";
+		$result = mysqli_query($mysql, $query);
+		while ($row = mysqli_fetch_assoc($result)) {
+			if ($_SESSION['login'] == $row['Login']) {
+				return "garant"; // TODO
+			}
+		}
+		$query = "SELECT Login FROM Osoba NATURAL JOIN Student";
+		$result = mysqli_query($mysql, $query);
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			if ($_SESSION['login'] == $row['Login']) {
+				return "student";
+			}
+		}
+	}
+
 	public function show_attributes()
 	{
 		echo "<pre>";
 		echo "login: " . $this->login . "\npassword: " . $this->password . "\n";
 		echo "</pre>";
+	}
+}
+
+class Swal_select {
+	public function __construct($type, $title, $message)
+	{
+		if ($type == "alert") {
+
+		} else if ($type == "warning") {
+
+		} else if  ($type == "success") {
+
+		}
 	}
 }
 
--- index.php
+++ index.php
@@ -5,34 +5,18 @@
 <link href="./html/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-	<?php
-	if (isset($_POST["input_login"]) && isset($_POST["input_password"])) {
-		require_once('./html/php/class.php');
-		$login_class = new Login();
-
-		if (!$login_class->compare_password()) {
-			// TODO swal
-		} else {
-			//subtract new timestamp from the old one
-			if(time() - $_SESSION['timestamp'] > 900) {
-			    echo"<script>alert('15 Minutes over!');</script>";
-			    unset($_SESSION['username'], $_SESSION['password'], $_SESSION['timestamp']);
-			    $_SESSION['logged_in'] = false;
-			    header("Location: http://www.stud.fit.vutbr.cz/~xvasko12/IIS/"); //redirect to index.php
-			} else {
-			    $_SESSION['timestamp'] = time(); //set new timestamp
-			    set($_SESSION['input_login'], $_SESSION['input_password'], $_SESSION['timestamp']);
-			}
-		}
-		die();
-	}
-	?>
+<?php
+session_start();
+require_once('./html/php/class.php');
+require_once("./html/php/session.php"); // creates $login_class class
+if (!isset($_SESSION['login']) && !isset($_SESSION['logged_in'])) {
+?>
 <body background="./img/clouds.jpg" style="background-repeat: no-repeat;background-size: 100%;">
 	<div class="container" style="max-width: 300px; margin-top: 10%; margin-bottom:10%;">
 		<form method=POST>
 			<div class="form-group">
 				<label for="exampleInputLogin">Login</label>
-				<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter login" name="input_login">
+				<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter login" name="input_login" value="<?php if($save === 1) echo $_POST["input_login"];?>">
 				<small id="emailHelp" class="form-text text-muted">We'll never share your login with anyone else.</small>
 			</div>
 
@@ -40,16 +24,34 @@
 				<label for="exampleInputPassword1">Password</label>
 				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="input_password">
 			</div>
-
-
-			<!-- TODO reload page with new one
-				<div class="form-check">
-				<label class="form-check-label">
-				<input type="checkbox" class="form-check-input">
-				Check me out
-				</label>
-			</div> -->
 			<button type="submit" class="btn btn-primary">Login</button>
 		</form>
 	</div>
 </body>
+<?php
+} else {
+	require_once('./html/php/class.php');
+	$login_class = new Login();
+	$login_class->check_session();
+	// echo "<pre>" . $login_class->get_user() . "\n</pre>";
+	// TODO rozdel medzi administratora, garanta a studenta
+?>
+
+<body background="./img/clouds.jpg" style="background-repeat: no-repeat;background-size: 100%;">
+	<div class="container" style="max-width: 300px; margin-top: 10%; margin-bottom:10%;">
+		<form method=POST>
+			<!-- TODO reload page with new one -->
+			<div class="form-check">
+				<label class="form-check-label">
+				<input type="checkbox" class="form-check-input">
+				Check me out
+				</label>
+			</div>
+			<button type="submit" class="btn btn-primary">Press</button>
+		</form>
+	</div>
+</body>
+
+<?php
+}
+?>
